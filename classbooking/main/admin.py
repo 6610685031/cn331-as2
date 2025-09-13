bfbba6945a5db0974046e99590ada5b49d24f820
Krentiz <48907723+Krentiz@users.noreply.github.com>
2025-09-13 20:08:09 +0700

    make superusers able to access models in admin page

--- classbooking/main/admin.py
+++ classbooking/main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Classroom, Booking
 
-# Register your models here.
+
+@admin.register(Classroom)
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ("name", "capacity", "is_available", "booked_by")
+    list_filter = ("is_available",)
+    search_fields = ("name", "booked_by__username")
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("classroom", "user", "start_time", "end_time")
+    list_filter = ("classroom", "user")
+    search_fields = ("classroom__name", "user__username")
+    date_hierarchy = "start_time"
