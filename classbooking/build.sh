7f269a29a878ae309b65b3cf0495164aa6a8a07b
Krentiz <48907723+Krentiz@users.noreply.github.com>
2025-09-20 01:59:39 +0700

    remove password field from build.sh

--- classbooking/build.sh
+++ classbooking/build.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
  
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
+python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
