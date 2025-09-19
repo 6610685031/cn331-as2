9b5e143f830681872e0284873d33feb0d0b6192f
Krentiz <48907723+Krentiz@users.noreply.github.com>
2025-09-20 02:05:24 +0700

    flush database on each deploy

--- classbooking/build.sh
+++ classbooking/build.sh
@@ -4,7 +4,8 @@ set-o errexit
 
 pip install --upgrade pip
 pip install -r requirements.txt
- 
+
+python manage.py flush --no-input
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
