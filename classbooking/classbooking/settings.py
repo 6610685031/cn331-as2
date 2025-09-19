bcedfc221ce999314e87bc8f7352783ee9a03b37
Krentiz <48907723+Krentiz@users.noreply.github.com>
2025-09-20 03:07:32 +0700

    add thai translation for embled description

--- classbooking/classbooking/settings.py
+++ classbooking/classbooking/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "default-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-# allow other devices to use the website for local testing
 ALLOWED_HOSTS = ["*"]
 
 
